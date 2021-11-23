24612c123e687747252806d040ebff3be2cab83a
“Varun13016” <dmevarun@gmail.com>
2021-11-23 10:56:08 +0530

    uc10

--- LinkedListDemo/Program.cs
+++ LinkedListDemo/Program.cs
@@ -10,26 +10,16 @@ namespace LinkedListDemo
             List<int> L1 = new List<int>();
             L1.Add(56);
             L1.Add(30);
-            L1.Add(40);
             L1.Add(70);
+            L1.Add(40);
 
             foreach (var element in L1)
             {
                 Console.WriteLine(element);
             }
 
-            Console.WriteLine("----------After----");
-            int size = L1.Count;
-            for (int i = 0; i < size; i++)
-            {
-                if (L1[i] == 40) ;
-                {
-                    L1.RemoveAt(i);
-                    break;
-                }
-            }
-
-            Console.WriteLine("Size is " + L1.Count);
+            Console.WriteLine("-------------After sorting -----------------");
+            L1.Sort();
             foreach (var element in L1)
             {
                 Console.WriteLine(element);
