a8af988058b03a37e0e983c2159b897dd76800c1
“Varun13016” <dmevarun@gmail.com>
2021-11-23 10:51:51 +0530

    uc6

--- LinkedListDemo/Program.cs
+++ LinkedListDemo/Program.cs
@@ -11,16 +11,9 @@ namespace LinkedListDemo
             L1.Add(56);
             L1.Add(30);
             L1.Add(70);
-            foreach (var element in L1)
-            {
-                Console.WriteLine(element);
-            }
-            L1.RemoveAt(0);
-            Console.WriteLine("Afetr removing first element------------");
-            foreach (var element in L1)
-            {
-                Console.WriteLine(element);
-            }
+            L1.Add(99);
+
+            Console.WriteLine(L1.Contains(30));
 
         }
     }
