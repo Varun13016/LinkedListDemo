68f417ee74d98cebf6bebaabd714a7c81222e0b6
“Varun13016” <dmevarun@gmail.com>
2021-11-23 10:45:26 +0530

    uc2

--- LinkedListDemo/Program.cs
+++ LinkedListDemo/Program.cs
@@ -8,13 +8,14 @@ namespace LinkedListDemo
         static void Main(string[] args)
         {
             List<int> L1 = new List<int>();
-            L1.Add(56);
             L1.Add(70);
             L1.Add(30);
+            L1.Add(56);
             foreach (var element in L1)
             {
                 Console.WriteLine(element);
             }
+
         }
     }
 }
